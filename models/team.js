b5f4b2e36a19369a42627683b10270529e5f5520
Baltej Randhawa <baltej.randhawa0@gmail.com>
2022-04-26 09:40:26 -0700

    created Team model

---
+++ models/team.js
@@ -0,0 +1,19 @@
+var mongoose = require('mongoose')
+
+var Schema = mongoose.Schema;
+
+var TeamSchema = new Schema(
+    {
+        name:{type:String, required: true},
+        players:[{type: Schema.Types.ObjectId, ref: 'Player'}],
+        league:{type: Schema.Types.ObjectId, ref:'League'}
+    }
+);
+
+TeamSchema
+.virtual('url')
+.get(function(){
+    return '/league/team/'+this._id;
+})
+
+module.exports = mongoose.model('Team',TeamSchema)
