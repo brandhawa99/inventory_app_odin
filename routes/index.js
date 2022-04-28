97cbb28e2a4cdebdd7fde3447679f53a61aa2216
Baltej Randhawa <baltej.randhawa0@gmail.com>
2022-04-27 18:20:20 -0700

    made index be redirected to league

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'The Coolest Soccer Leagues' });
+// GET home page.
+router.get('/', function(req, res) {
+  res.redirect('/league');
 });
+
+
 
 module.exports = router;
