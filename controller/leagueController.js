892c6645877701c1b3cdb2a978fde1dda0a03131
Baltej Randhawa <baltej.randhawa0@gmail.com>
2022-04-27 18:19:15 -0700

    added league controller and created index function

---
+++ controller/leagueController.js
@@ -0,0 +1,33 @@
+var League = require('../models/league');
+var Player = require('../models/player');
+var Team = require('../models/team');
+
+var async = require('async');
+
+exports.index = function(req,res){
+  async.parallel({
+    leagues : function(callback){
+      League.countDocuments({}, callback)
+    },
+    players : function(callback){
+      Player.countDocuments({}, callback);
+    },
+    teams : function(callback){
+      Team.countDocuments({}, callback);
+    }
+  },  
+    function(err, results){
+      res.render('index', {title: 'Coolest Soccer Leagues Home', error: err, data: results})
+    }
+  )
+
+}
+
+
+exports.league_list = function(req,res,next){
+  League.find()
+  .exec(function(err,list_league){
+    if(err){return next(err);}
+    res.render('league_list',{title: 'Leagues List', author_list: list_league})
+  })
+};  
