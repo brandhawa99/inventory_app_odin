b446eb793560cf25f4979d479bae005d9309ff83
Baltej Randhawa <baltej.randhawa0@gmail.com>
2022-05-01 14:18:31 -0700

    created routes for league controller

--- routes/league.js
+++ routes/league.js
@@ -1,4 +1,5 @@
 var express = require('express');
+const { route } = require('express/lib/application');
 var router = express.Router();
 
 //Require controller modules.
@@ -8,8 +9,21 @@ var league_controller = require('../controller/leagueController');
 
 //GET HOME PAGE
 router.get('/', league_controller.index)
-router.get('/leagues',league_controller.league_list)
-router.get('/league/leagues/:id',league_controller.league_detail)
 
+router.post('/leagues/:id/delete', league_controller.league_delete_post)
+
+router.get('/leagues/:id/delete',league_controller.league_delete_get)
+
+router.post('/leagues/create',league_controller.league_create_post)
+
+router.get('/league/create',league_controller.league_create_get)
+
+router.post('/leagues/:id/update',league_controller.league_update_post)
+
+router.get('/leagues/:id/update',league_controller.league_update_get)
+
+router.get('/leagues/:id',league_controller.league_detail)
+
+router.get('/leagues',league_controller.league_list)
 
 module.exports = router ;
