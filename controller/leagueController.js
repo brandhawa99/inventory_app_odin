0d65aa9f332e4834f1185f6dc2f7558dbd60dfba
Baltej Randhawa <baltej.randhawa0@gmail.com>
2022-05-03 15:47:41 -0700

    implemented create post for league

--- controller/leagueController.js
+++ controller/leagueController.js
@@ -1,6 +1,7 @@
 var League = require('../models/league');
 var Player = require('../models/player');
 var Team = require('../models/team');
+const {body, validationResult} = require('express-validator');
 
 var async = require('async');
 
@@ -39,21 +40,58 @@ exports.league_detail = function(req,res,next){
   .populate('teams')
   .exec(function( err, results){
     if(err){return next(err)}
-    res.render('league_detail',{title: results.name, teams: results.teams})
+    res.render('league_detail',{title: results.name, teams: results.teams, league: results})
   })
-  // res.send('Todo implement detail page');
 }
 
-exports.league_create_post = function(req,res, next){
-  res.send('Need to be completed')
-}
+exports.league_create_post = [
+  //validate and sanitize the name field
+  body('name', "League name required").trim().isLength({min:1}).escape(),
+  //process request after validation and sanitization
+  (req,res,next)=>{
+    //Extract the validation errors from a request
+    const errors = validationResult(req);
+    // create a league object with escaped and trimmed data
+
+    var league = new League(
+      {name: req.body.name}
+    );
+    
+    if(!errors.isEmpty()){
+      //There are erros. Render the form again with sanitized data values/error messages.
+      res.render('league_form',{title:'Create New League', league: league,errors: errors.array()})
+      return;
+    }
+    else{
+      //Data from from is valid 
+      //check if genre with same name already exists
+      League.findOne({'name': req.body.name})
+        .exec(function(err, found_league){
+          if(err){return next(err)}
+          if(found_league){
+            res.redirect(found_league.url)
+          }else{
+            league.save(function(err){
+              if(err) {return next(err)}
+              res.redirect(league.url);
+            })
+          }
+        })
+    }
+  }
+
+]
 
 exports.league_create_get = function(req,res, next){
-  res.send('Need to be completed')
+  res.render('league_form',{title: 'Create New League'})
 }
 
 exports.league_delete_get = function(req,res,next){
-  res.send('Need to be completed');
+  League.findById(req.params.id)
+  .exec(function(err,result){
+    if(err){return next(err)}
+    res.render('league_delete',{title:'Delete this league', league: result})
+  })
 }
 
 exports.league_delete_post = function(req,res, next){
