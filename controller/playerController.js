90aa4589807007d499426f602206b412cb666048
Baltej Randhawa <baltej.randhawa0@gmail.com>
2022-05-03 15:48:00 -0700

    implemented player detail

--- controller/playerController.js
+++ controller/playerController.js
@@ -9,3 +9,27 @@ exports.player_list = function(req,res,next){
     res.render('player_list',{title: 'Players List', players: list_player})
   })
 }
+
+exports.player_detail= function(req,res,next){
+  Player.findById(req.params.id)
+  .exec(function(err, player){
+    if(err){return next(err)}
+    res.render('player_detail',{player:player})
+  })
+}
+
+exports.player_create = function(req,res,next){
+  res.send("need to implement")
+}
+
+exports.player_create_post = function(req,res,next){
+  res.send('need to implement')
+}
+
+exports.player_delete_get = function(req,res,next){
+  res.send('need to implement')
+}
+
+exports.player_delete_post = function(req,res,next){
+  res.send('need to implement')
+}
