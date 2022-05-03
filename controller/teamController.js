8a16463718618f4a7142869a897f0a2ac4bc7769
Baltej Randhawa <baltej.randhawa0@gmail.com>
2022-05-03 15:48:14 -0700

    implemented team detail

--- controller/teamController.js
+++ controller/teamController.js
@@ -7,3 +7,29 @@ exports.team_list = function(req,res,next){
     res.render('team_list',{title: 'Teams List', teams: list_team})
   })
 }
+
+
+exports.team_detail = function (req,res,next){
+  Team.findById(req.params.id)
+  .populate('players')
+  .exec(function(err, team_details){
+    if(err){return next(err)}
+    res.render('team_detail', {title: team_details.name, players: team_details.players, team:team_details})
+  })
+}
+
+exports.team_create = function(req,res,next){
+  res.send('need to implements')
+}
+
+exports.team_create_post = function(req,res,next){
+  res.send('need to implement')
+}
+
+exports.team_delete_get = function(req,res,next){
+  res.send('need to implmenet')
+}
+
+exports.team_delete_post= function (req,res,next){
+  res.send('need to implement');
+}
