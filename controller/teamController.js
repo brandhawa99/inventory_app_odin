7b58a0474f2542f63c2841212f0443d9969ffdb7
Baltej Randhawa <baltej.randhawa0@gmail.com>
2022-05-10 11:47:21 -0700

    team get/post for form

--- controller/teamController.js
+++ controller/teamController.js
@@ -25,17 +25,51 @@ exports.team_create = function(req,res,next){
   League.find()
     .exec(function(err,results){
       if(err){return next(err)}
-
       res.render('team_form',{title:'Create Team', leagues:results})
     })
 }
 
 exports.team_create_post = [
+  //sanitize
+  body('name').trim().isLength({min:1}).escape().withMessage("First name required.")
+    .isAlphanumeric().withMessage("First name has non-alphanumeirc characters."),
+  body('leagues.*',"Must select a League").equals(undefined).escape(),
+  body('wins').trim().escape().isNumeric().withMessage('Must be a number'),
+  body('losses').trim().escape().isNumeric().withMessage('Must be a number'),
+
+  (req,res,next) => {
+    const errors = validationResult(req);
+    
+    if(!errors.isEmpty()){
+      res.render('team_form',{title: 'Create Team', team: req.body, erors: errors.array()})
+    }else{
+      var team = new Team({
+        name: req.body.name,
+        players:[],
+        wins: req.body.wins,
+        loss: req.body.losses,
+
+      })
+      team.save(function(err){
+        if(err){return next(err)}
+        League.findByIdAndUpdate(req.body.leagues,{$push:{'teams':team}},()=>{
+          if(err){return next(err)}
+          res.redirect(team.url);
+        })
+      })
+
+    }
+
+  }
 
 ]
 
 exports.team_delete_get = function(req,res,next){
-  res.send('need to implmenet')
+  Team.findById(req.params.id)
+    .exec(function(err,results){
+      if(err){return next(err)}
+      res.render('team_delete',{title: "Delete this team",team:results})
+    })
 }
 
 exports.team_delete_post= function (req,res,next){
